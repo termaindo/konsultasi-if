37af9f0ac8fa9a30837533ec807fe98b7e711f64
termaindo <terma.indo@gmail.com>
2026-05-07 17:39:44 +0700

    Update source link in nutrisi.py

--- modules/nutrisi.py
+++ modules/nutrisi.py
@@ -37,7 +37,7 @@ def create_pdf_nutrisi(teks_analisa, nama_user, usia_user, logo_path="Logo_Aplik
     pdf.set_y(35)
     pdf.set_font("Arial", 'I', 10)
     pdf.set_text_color(0, 0, 255)  
-    pdf.cell(0, 5, "Sumber: https://aplikasisehat.streamlit.app", ln=True, align='C', link="https://aplikasisehat.streamlit.app")
+    pdf.cell(0, 5, "Sumber: https://s.id/aplikasisehat", ln=True, align='C', link="https://s.id/aplikasisehat")
     pdf.ln(2)
     
     pdf.set_text_color(0, 0, 0) 
